2448454377b27ca87b181a196b85ce8eed63eec4
System Administrator <boris_moers@rcomext.com>
2019-11-19 12:44:04 +0100

    Changes by 'System Administrator' on 'A Global Scoped App - 1.1.1' -- Build #1

---
+++ sn/A Global Scoped App/Dictionary Entry/u_a_global_scoped_app_table/active_true/u_column_a/calculation.js
@@ -0,0 +1,15 @@
+/* 
+ * Application : A Global Scoped App
+ * ClassName   : sys_dictionary
+ * Created On  : 2019-09-11 15:26:50
+ * Created By  : admin
+ * Updated On  : 2019-09-11 15:26:50
+ * Updated By  : admin
+ * URL         : /sys_dictionary.do?sys_id=6dc3868edbff7700fcf4178039961918
+ */
+(function calculatedFieldValue(current) {
+
+	// Add your code here
+	return '';  // return the calculated value
+
+})(current);
